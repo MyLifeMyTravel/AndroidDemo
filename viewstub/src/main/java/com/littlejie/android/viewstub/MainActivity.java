835c39bc2cdf6e21ef49befcd756b9c4f160d356
littlejie <lsj_lishengjie@163.com>
2016-06-06 17:55:25 +0800

    辅助功能和ViewStub

--- viewstub/src/main/java/com/littlejie/android/viewstub/MainActivity.java
+++ viewstub/src/main/java/com/littlejie/android/viewstub/MainActivity.java
@@ -1,13 +1,27 @@
 package com.littlejie.android.viewstub;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewStub;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTvShowStub;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTvShowStub = (TextView) findViewById(R.id.btn_show_viewstub);
+        mTvShowStub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.viewstub).setVisibility(View.VISIBLE);
+//                ((ViewStub) findViewById(R.id.viewstub)).inflate();
+            }
+        });
     }
 }
