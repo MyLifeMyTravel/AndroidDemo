b422136aab4afdc46702de3caf9e7b1cccc73c82
littlejie <lsj_lishengjie@163.com>
2016-07-15 13:17:50 +0800

    添加沉浸式状态栏支持

---
+++ littlejie/src/main/java/com/littlejie/utils/TranslucentBarUtil.java
@@ -0,0 +1,34 @@
+package com.littlejie.utils;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+
+import com.littlejie.manager.SystemBarTintManager;
+
+/**
+ * Created by Lion on 2016/7/15.
+ */
+public class TranslucentBarUtil {
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    public static void setTranslucentBar(@NonNull Activity activity, int color) {
+        activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        ViewGroup rootView = (ViewGroup) activity.getWindow().getDecorView();
+        rootView.setFitsSystemWindows(true);
+        rootView.setClipToPadding(true);
+        SystemBarTintManager tintManager = new SystemBarTintManager(activity);
+        tintManager.setStatusBarTintEnabled(true);
+        tintManager.setStatusBarTintColor(color);
+    }
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    public static void setTranslucentBar(@NonNull Activity activity, String color) {
+        int parseColor = Color.parseColor(color);
+        setTranslucentBar(activity, parseColor);
+    }
+}
