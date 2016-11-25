de9a8cc231d7cf1f8de16e0d2d765894897b9833
littlejie <lsj_lishengjie@163.com>
2016-11-25 10:02:56 +0800

    canvas

---
+++ customview/src/main/java/com/littlejie/customview/CanvasDemoActivity.java
@@ -0,0 +1,19 @@
+package com.littlejie.customview;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by littlejie on 2016/11/22.
+ */
+
+public class CanvasDemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_canvas_demo);
+    }
+
+}
